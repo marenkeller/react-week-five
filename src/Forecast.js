387056e3ace0e44a396bb9535c72b14980f4207f
Maren Keller <104692152+marenkeller@users.noreply.github.com>
2023-01-25 19:17:28 +0100

    removed dummy forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,18 +1,15 @@
 import React from "react";
 import "./Forecast.css";
 
-export default function Forecast() {
+export default function Forecast(props) {
   return (
-    <div className="row">
+    <div className="WeatherForecastrow">
       <div className="col-2">
-        <div>Mon</div> <span className="ForecastTempMax">3</span>{" "}
-        <span className="ForecastTempMin">-2</span>
+        <div>Mon</div>
+        <div>☃️</div>
+        <span className="ForecastTempMax">3°</span>{" "}
+        <span className="ForecastTempMin">-2°</span>
       </div>
-      <div className="col-2">Tue</div>
-      <div className="col-2">Wed</div>
-      <div className="col-2">Thu</div>
-      <div className="col-2">Fri</div>
-      <div className="col-2">Sat</div>
     </div>
   );
 }
